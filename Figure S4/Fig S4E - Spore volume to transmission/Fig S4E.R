608ad0a60198739213de105c191d115f4da1002b
Jason-B-Jiang <79412167+Jason-B-Jiang@users.noreply.github.com>
2021-05-10 12:58:21 -0400

    Add files via upload

--- Figure S4/Fig S4E - Spore volume to transmission/Fig S4E.R
+++ Figure S4/Fig S4E - Spore volume to transmission/Fig S4E.R
@@ -3,7 +3,7 @@
 # Compare spore volume to transmission
 #
 # Jason Jiang - Created: 2020/08/16
-#                     Last Edit: 2021/02/19
+#                     Last Edit: 2021/05/09
 #
 # Reinke Lab - Microsporidia Database Project
 #
@@ -20,7 +20,7 @@ library(readxl)
 
 
 # Set the working directory, this is what I use.
-setwd('P:/Shared/Microsporidia database/Figure S4 (Jason)/Fig S4E - Spore volume to transmission')
+setwd('P:/Shared/Microsporidia database/Figure S4/Fig S4E - Spore volume to transmission')
 
 
 # Load in the database I described earlier, of microsporidia species and their
@@ -29,11 +29,17 @@ dimension_infection_db <- read_xlsx("NEW VOLUMES DATABASE.xlsx")
 
 
 # Filter out species without transmission data
+strip_parentheses <- function(x) {
+  # Function to remove parentheses from volume 
+  strsplit(trimws(strsplit(x, "\\(")[[1]][1]), " ")[[1]][1]
+}
+
+strip_parentheses <- Vectorize(strip_parentheses)
+
 dimension_infection_db <- dimension_infection_db %>%
   filter(!is.na(Transmission) & !is.na(Volume)) %>%
   separate_rows(Volume, sep = "; ") %>%
-  rowwise() %>%
-  mutate(Volume_formatted = as.numeric(str_split(Volume, " ")[[1]][1])) %>%
+  mutate(Volume_formatted = as.numeric(strip_parentheses(Volume))) %>%
   arrange(Volume_formatted)
 
 # First, perform Kruskal-Wallis test to see if any median tube values are significantly
@@ -45,10 +51,10 @@ kruskal.test(data = dimension_infection_db, log10(Volume_formatted) ~ Transmissi
 ggplot(dimension_infection_db, aes(x = Transmission,
                                  y = Volume_formatted,
                                  fill = Transmission)) +
-  geom_boxplot(show.legend = FALSE, lwd = 0.2) +
+  geom_boxplot(show.legend = FALSE, lwd = 0.7) +
   scale_y_log10() +
   theme_bw() +
-  labs(x = "Transmission", y = "log_Volume", title = "\np = 0.14") +
+  labs(x = "Transmission", y = "log_Volume", title = "\np = 0.15") +
   theme(plot.title = element_text(size = 18),
         axis.text = element_text(size = 18, color= "black"),
         axis.title = element_text(size = 18),
