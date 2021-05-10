3edc474a9daf5e9f29ad58c89bc75d91a9b5fd22
Jason-B-Jiang <79412167+Jason-B-Jiang@users.noreply.github.com>
2021-05-10 12:57:22 -0400

    Add files via upload
    Updated figure S4C/D spreadsheets + scripts

--- Figure S4/Fig S4C + S4D - Polar tube and transmission/Fig S4C + S4D.R
+++ Figure S4/Fig S4C + S4D - Polar tube and transmission/Fig S4C + S4D.R
@@ -3,7 +3,7 @@
 # Compare tube lengths to infection
 #
 # Jason Jiang - Created: 2020/08/14
-#                     Last Edit: 2021/02/26
+#                     Last Edit: 2021/05/09
 #
 # Reinke Lab - Microsporidia Database Project
 #
@@ -20,7 +20,7 @@ library(readxl)
 
 
 # Set working directory
-setwd("P:/Shared/Microsporidia database/Figure S4 (Jason)/Fig S4C + S4D - Polar tube and transmission")
+setwd("P:/Shared/Microsporidia database/Figure S4/Fig S4C + S4D - Polar tube and transmission")
 
 
 # Load in spreadsheets containing relevant data
@@ -43,22 +43,22 @@ strip_parentheses <- function(tube_value) {
   strsplit(trimws(strsplit(tube_value, "\\(")[[1]][1]), " ")[[1]][1]
 }
 
-vstrip_parentheses <- Vectorize(strip_parentheses)
+strip_parentheses <- Vectorize(strip_parentheses)
 
 
 # Write functions for formatting the tube data, so all parenthesized text is
 # stripped from the tube data.
 clean_tube_data <- function(tube_data) {
   raw_values <- strsplit(tube_data, "; ")[[1]]
-  paste(vstrip_parentheses(raw_values), collapse = "; ")
+  paste(strip_parentheses(raw_values), collapse = "; ")
 }
 
+clean_tube_data <- Vectorize(clean_tube_data)
 
 # Separate transmission_tube_db into 2 dataframes, one for calculated tube values
 # only and the other for experimental tube values
 exp <- transmission_tube_db %>%
   filter(!is.na(Tube_act)) %>%
-  rowwise() %>%
   mutate(Tube_act = clean_tube_data(Tube_act)) %>%
   separate_rows(Tube_act, sep = "; ") %>%
   mutate(Tube_act = as.numeric(Tube_act)) %>%
@@ -66,7 +66,6 @@ exp <- transmission_tube_db %>%
 
 calc <- transmission_tube_db %>%
   filter(!is.na(Tube_calc)) %>%
-  rowwise() %>%
   mutate(Tube_calc = clean_tube_data(Tube_calc)) %>%
   separate_rows(Tube_calc, sep = "; ") %>%
   mutate(Tube_calc = as.numeric(Tube_calc)) %>%
@@ -86,14 +85,14 @@ ggplot(exp, aes(x = Transmission, y = Tube_act, fill = Transmission)) +
   theme(plot.title = element_text(size = 18),
         axis.text = element_text(size = 18, color = "black"),
         axis.title = element_text(size = 18),
-        axis.title.x = element_blank(),
+        axis.title.x = element_blank(),???
         panel.background = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
 ggplot(calc, aes(x = Transmission, y = Tube_calc, fill = Transmission)) +
   geom_boxplot(show.legend = FALSE) +
-  labs(x = "Transmission", y = "Polar Tube Length (\u03bcm)", title = "Calculated Polar Tubes\np = 0.32") +
+  labs(x = "Transmission", y = "Polar Tube Length (\u03bcm)", title = "Calculated Polar Tubes\np = 0.30") +
   theme_bw() +
   theme(plot.title = element_text(size = 18),
         axis.text = element_text(size = 18, color = "black"),
